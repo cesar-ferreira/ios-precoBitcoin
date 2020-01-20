63d3ff0e3c512b4a8f3c912c51b0d1826b3c4c36
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-01-20 07:14:12 -0300

    implement request

--- preço bitcoin/preço bitcoin/ViewController.swift
+++ preço bitcoin/preço bitcoin/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelPrecoBitcoin: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.getPriceBitcoin()
     }
 
+    @IBAction func refresh(_ sender: Any) {
+        self.getPriceBitcoin()
+    }
+    
+    private func priceFormatter(price: NSNumber) -> String {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let priceFinal = nf.string(from: price) {
+            return priceFinal
+        }
+        
+        return "0,00"
+    }
+    
+    private func getPriceBitcoin() {
+        print("AQUI")
+        
+        self.button.setTitle("Atualizando...", for: .normal)
+        if let url = URL(string: "https://blockchain.info/pt/ticker") {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    
+                if (error == nil) {
+                    if let dataRequest = data {
+                        do {
+                            if let objectJson = try JSONSerialization.jsonObject(with: dataRequest, options: []) as? [String: Any] {
+                                if let brl = objectJson["BRL"] as? [String: Any] {
+                                    if let price = brl["buy"] as? Double {
+                                        let priceFormatter = self.priceFormatter(price: NSNumber(value: price))
+                                        
+                                        print(priceFormatter)
+                                        
+                                        DispatchQueue.main.async(execute: {
+                                            self.labelPrecoBitcoin.text = "R$ " + priceFormatter
 
+                                        })
+                                    }
+                                }
+                            }
+                        } catch  {
+                            print("Erro ao formatar o retorno.")
+                        }
+                    }
+                }
+                print("ERROR " + error.debugDescription)
+
+            }
+            task.resume()
+        }
+    }
+    
+    
+    
 }
 
